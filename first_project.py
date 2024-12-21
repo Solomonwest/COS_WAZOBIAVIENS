81378eee09c2118136b048dda4aba7306757efda
uzorsamuel <samuzor7@gmail.com>
2024-12-21 19:01:09 +0000

    samuel igbo

--- first_project.py
+++ first_project.py
@@ -2,6 +2,34 @@ import tkinter as tk
 
 windows = tk.TK()
 
+igbo_dictionary = {
+    "hello": "Ndewo",
+    "goodbye": "Ka chi fo",
+    "thank you": "Imela",
+    "how are you": "Kedu?",
+    "i'm fine": "A di mma",
+    "water": "Mmiri",
+    "food": "Nri",
+    "house": "Ụlọ",
+    "car": "Oche",
+    "tree": "Osu",
+    "love": "Ịhụnanya",
+    "family": "Ụmụnne",
+    "friend": "Enyi",
+    "mother": "Nne",
+    "father": "Nna",
+    "brother": "Nwanne",
+    "sister": "Nwanyị",
+    "yes": "Ee",
+    "no": "Mba",
+    "maybe": "Inwere",
+    "excuse me": "Kedu kedu",
+    "sorry": "Mgbaghara",
+    "good morning": "Ụtụtụ ọma",
+    "good afternoon": "Ehihie ọma",
+    "good evening": "Ụchi ọma",
+    "good night": "Ụlọ ọma"
+}
 
 
 
