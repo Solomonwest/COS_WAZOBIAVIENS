903bc4923f4694f0e92bcb82043f844463fb36c7
Dannii-wweb <danielajayi599@gmail.com>
2024-12-21 21:12:43 +0100

    Daniel_yoruba

--- first_project.py
+++ first_project.py
@@ -123,7 +123,38 @@ igbo_words = {
     "Chọta": "find"
 }
 
-
+yoruba_dictionary = {
+    "Stand":'Duro',
+    "Joy":'Ayo',
+    "Healthy":'Alafia',
+    "Light":'Imole',
+    "Sit":'Jokoo',
+    "Black":'Dudu',
+    "Lies":'Iro',
+    "Sickness":'Aisan',
+    "Fire":'Ina',
+    "world":'Aye',
+    "Earth":'Ile',
+    "Air":'Afefe',
+    "Water":'Omi',
+    "Good morning":'Ekaro',
+    "Love":'Ife',
+    "Wealth":'Ola',
+    "God":'Oluwa',
+    "Rest":'Simi',
+    "Enjoyment":'igbadun',
+    "woman":'Obirin',
+    "Come":'Wa',
+    "Child":'Omo',
+    "Man":'Okurin',
+    "Cap":'Fila',
+    "House":'Ile',
+    "Beans":'Ewa',
+    "Friend":'Ore',
+    "Festival":'Odun',
+    "Crown":'Ade',
+    "Clock":'Ago',
+}
 
 
 
