e5d5e56be721fb2a3245a3013d22d476e408580a
Aaronjames <aikrooz228>
2024-12-22 09:47:03 -0800

    Aaron hausa

--- first_project.py
+++ first_project.py
@@ -178,5 +178,33 @@ yoruba_dictionary = {
 
 
 
+Hausa={
+    'Gida': 'House',
+    'Ruwa': 'Water',
+    'Abinci': 'Food',
+    'Mutum': 'Person',
+    'Yaro': 'Boy',
+    'Yarinya': 'Girl',
+    'Kudi': 'Money',
+    'Littafi': ' Book',
+    'Makaranta': 'School',
+    'Kaya': 'Clothes',
+    'Duniya': 'World',
+    'Rana': 'Sun',
+    'Dare': 'Night',
+    'Zuciya': 'Heart',
+    'Rafi': 'River',
+    'Uba': 'Father',
+    'Yawa': 'Many',
+    'Lafiya': 'Health',
+    'Rubutu': 'Write',
+    'Hanya': 'Road',
+    'yunwa':'hunger'
+}
+
+
+
+
+
 
 windows.mainloop()
